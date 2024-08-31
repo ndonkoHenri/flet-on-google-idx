6ca2ef52c5601e1d57de692775fbc1b6c6838dea
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2024-08-31 11:52:59 +0200

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,53 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ pkgs.python3 ];
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        create-venv = ''
+          python -m venv .venv
+          source .venv/bin/activate
+          pip install -r requirements.txt
+        '';
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "main.py" ];
+      };
+      onStart = {
+        # check the existence of the venv and create if non existent
+        check-venv-existence = ''
+          VENV_DIR=".venv"
+          if [ ! -d "$VENV_DIR" ]; then
+            echo "Virtual environment not found. Creating one..."
+            python -m venv "$VENV_DIR" && source "$VENV_DIR"/bin/activate && pip install -r requirements.txt
+            echo "Virtual environment created in $VENV_DIR."
+          else
+              echo "Virtual environment already exists in $VENV_DIR."
+              source "$VENV_DIR"/bin/activate
+          fi
+        '';
+      };
+
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "./preview.sh" ];
+          env = {
+            PORT = "$PORT";
+          };
+          manager = "web";
+        };
+      };
+    };
+  };
+}
