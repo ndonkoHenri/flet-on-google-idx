fa1567b46b3ecdfbc00d108a5b3395e24d96348d
ndonkoHenri <robotcoder4@protonmail.com>
2024-09-07 16:42:56 +0200

    update files

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
+    def handle_minus_click(e: ft.ControlEvent):
+        counter.value = str(int(counter.value) - 1)
         page.update()
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
+    def handle_plus_click(e: ft.ControlEvent):
+        counter.value = str(int(counter.value) + 1)
         page.update()
+
+    counter = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
     page.add(
         ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
             alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.IconButton(ft.icons.REMOVE, on_click=handle_minus_click),
+                counter,
+                ft.IconButton(ft.icons.ADD, on_click=handle_plus_click),
+            ],
         )
     )
+
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
