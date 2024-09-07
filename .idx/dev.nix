88ace47c933ba209276eaa6cb29259b5824003ca
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2024-09-07 16:33:55 +0200

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,11 +1,10 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+
   packages = [ pkgs.python3 ];
 
+  # environment variables
   env = {
     VENV_DIR = ".venv";
   };
@@ -13,6 +12,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [ "ms-python.python" ];
+
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
@@ -20,34 +20,60 @@
         create-venv = ''
           python -m venv $VENV_DIR
           source $VENV_DIR/bin/activate
+          
+          if [ ! -f requirements.txt ]; then
+            echo "requirements.txt not found. Creating one with flet..."
+            echo "flet" > requirements.txt
+          fi
+
           pip install -r requirements.txt
         '';
 
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "main.py" "README.md" ];
       };
+
       onStart = {
-        # check the existence of the venv and create if non existent
+        # check the existence of the venv and create if non-existent
         check-venv-existence = ''
           if [ ! -d $VENV_DIR ]; then
             echo "Virtual environment not found. Creating one..."
-            python -m venv $VENV_DIR && source $VENV_DIR/bin/activate && pip install -r requirements.txt
-            echo "Virtual environment created in $VENV_DIR."
+            python -m venv $VENV_DIR
           fi
+
+          if [ ! -f requirements.txt ]; then
+            echo "requirements.txt not found. Creating one with flet..."
+            echo "flet" > requirements.txt
+          fi
+          
+          # activate virtual env and install requirements
+          source $VENV_DIR/bin/activate
+          pip install -r requirements.txt
         '';
       };
     };
+
     # Enable web preview
     previews = {
       enable = true;
       previews = {
         web = {
-          command = [ "./preview.sh" ];
           # cwd = "subfolder"
+          command = [
+            "bash"
+            "-c"
+            ''
+            # activate the virtual environment
+            source $VENV_DIR/bin/activate
+            
+            # run app in hot reload mode on a port provided by IDX
+            flet run main.py --web --port $PORT
+            ''
+          ];
           env = { PORT = "$PORT"; };
           manager = "web";
         };
       };
     };
   };
-} 
+}
