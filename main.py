948aa0c9761e61bc14f7d78e8b78295d2230ec2b
ndonkoHenri <robotcoder4@protonmail.com>
2024-09-07 17:03:21 +0200

    update main.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Flet on Google IDX"
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.appbar = ft.AppBar(
+        title=ft.Text("Flet on Google IDX"),
+        center_title=True,
+        bgcolor=ft.colors.LIGHT_BLUE_600,
+    )
 
     def handle_minus_click(e: ft.ControlEvent):
         counter.value = str(int(counter.value) - 1)
@@ -23,7 +30,8 @@ def main(page: ft.Page):
                 counter,
                 ft.IconButton(ft.icons.ADD, on_click=handle_plus_click),
             ],
-        )
+        ),
+        ft.Text("Made with ❤️ by TheEthicalBoy!", size=13),
     )
 
 
